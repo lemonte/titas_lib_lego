4aecf4ede220c59170ddcae68f89e6499f67b5cf
Geanderson <48225849+lemonte@users.noreply.github.com>
2024-06-27 17:39:26 -0300

    Update robo_imports.py

--- titas_lib/robo_imports.py
+++ titas_lib/robo_imports.py
@@ -1,105 +1,109 @@
 # robo_imports.py
 from titas_lib.hub_base import RoboHub
 
+Motor = None
+UltrasonicSensor = None
+ColorSensor = None
+GyroSensor = None
+LUMPDevice = None
+DCMotor = None
+Port = None
+Stop = None
+Direction = None
+DriveBase = None
+EV3Brick = None
+Color = None
+Button = None
+Icon = None
+Side = None
 
+def load_hub(hub_type):
+    global Motor, UltrasonicSensor, ColorSensor, GyroSensor, LUMPDevice, DCMotor, Port, Stop, Direction, DriveBase, EV3Brick, Color, Button, Icon, Side
+    if hub_type == RoboHub.EV3BRICK:
+        from pybricks.ev3devices import Motor, UltrasonicSensor, ColorSensor, GyroSensor
+        from pybricks.parameters import Port, Stop, Direction, Color, Button, Icon, Side
+        from pybricks.robotics import DriveBase
+        from pybricks.iodevices import LUMPDevice, DCMotor
+        from pybricks.hubs import EV3Brick
+    elif hub_type == RoboHub.SPIKEHUB:
+        from pybricks.pupdevices import Motor, ColorSensor, ColorDistanceSensor, UltrasonicSensor, ForceSensor
+        from pybricks.parameters import Port, Stop, Direction, Color, Button, Icon, Side
+        from pybricks.robotics import DriveBase
+        from pybricks.hubs import PrimeHub
+    else:
+        raise ValueError("Tipo de hub não suportado.")
 
 class RoboImports:
-    hub = None 
-    hub_type= None
+    hub = None
+    hub_type = None
 
     def __init__(self, hub_type):
         self.hub_type = hub_type
-        self.hub = self.load_hub()
+        load_hub(hub_type)
+        self.hub = self.get_hub()
 
-    def load_hub(self):
+    def get_hub(self):
         if self.hub_type == RoboHub.EV3BRICK:
-            global Motor, UltrasonicSensor,LUMPDevice,DCMotor, ColorSensor, GyroSensor, Port, Stop, Direction, DriveBase, EV3Brick,  Color, Button,Icon, Side
-            from pybricks.ev3devices import Motor, UltrasonicSensor, ColorSensor, GyroSensor
-            from pybricks.parameters import Port, Stop, Direction, Color, Button,Icon, Side
-            from pybricks.robotics import DriveBase
-            from pybricks.iodevices import LUMPDevice, DCMotor
-            from pybricks.hubs import EV3Brick
             return EV3Brick()
-
         elif self.hub_type == RoboHub.SPIKEHUB:
-            global Motor, ColorSensor, ColorDistanceSensor, UltrasonicSensor, ForceSensor, Port, Stop, Direction, DriveBase, PrimeHub,  Color, Button,Icon, Side
-            from pybricks.pupdevices import Motor, ColorSensor, ColorDistanceSensor, UltrasonicSensor, ForceSensor
-            from pybricks.parameters import Port, Stop, Direction, Color, Button,Icon, Side
-            from pybricks.robotics import DriveBase
-            from pybricks.hubs import PrimeHub
             return PrimeHub()
-
         else:
             raise ValueError("Tipo de hub não suportado.")
 
-      
     def get_motor(self, port: str):
         return Motor(self.__definirPorta(Port=port))
 
     def getGiroscopico(self, port: str):
         return GyroSensor(self.__definirPorta(port))
 
-    
     def getStop(self):
         """ Retorna a classe de Stop do lego. """
         return Stop
-    
-    
+
     def getColorSensor(self, Port: str):
         return ColorSensor(self.__definirPorta(Port))
 
-    
-    
     def getLUMPDevice(self, Port: str):
-            return LUMPDevice(self.__definirPorta(Port=Port))
-
-
+        return LUMPDevice(self.__definirPorta(Port=Port))
 
     def getUltrasonicSensor(self, Port: str):
-            return UltrasonicSensor(self.__definirPorta(Port=Port))
-
+        return UltrasonicSensor(self.__definirPorta(Port=Port))
 
     def getDCMotor(self, Port: str):
         """ Retorna o objeto DC do Motor do lego """
         return DCMotor(self.__definirPorta(Port=Port))
-    
 
     def getDirection(self):
         """ Retorna a classe Direction do lego """
         return Direction
 
-
-    def getDriveBase(self, motorEsquerdo, motorDireito, diametroRoda: float, distanciaEntreAsRodas: int ):
+    def getDriveBase(self, motorEsquerdo, motorDireito, diametroRoda: float, distanciaEntreAsRodas: int):
         """ Retorna o objeto DriveBase do lego """
         return DriveBase(motorEsquerdo.getMotor(), motorDireito.getMotor(), wheel_diameter=diametroRoda, axle_track=distanciaEntreAsRodas)
-
 
     def getPorta(self):
         """ Retorna a classe com a porta desejada do lego """
         return Port
-    
-    
+
     def __definirPorta(self, Port: str):
         """ Faz tratativa para a porta do lego """
         porta = None
-        if(Port.upper() == "A"):
+        if Port.upper() == "A":
             porta = self.getPorta().A
-        elif(Port.upper() == "B"):
+        elif Port.upper() == "B":
             porta = self.getPorta().B
-        elif(Port.upper() == "C"):
+        elif Port.upper() == "C":
             porta = self.getPorta().C
-        elif(Port.upper() == "D"):
+        elif Port.upper() == "D":
             porta = self.getPorta().D
-        elif(Port.upper() == "1"):
+        elif Port.upper() == "1":
             porta = self.getPorta().S1
-        elif(Port.upper() == "2"):
+        elif Port.upper() == "2":
             porta = self.getPorta().S2
-        elif(Port.upper() == "3"):
+        elif Port.upper() == "3":
             porta = self.getPorta().S3
-        elif(Port.upper() == "4"):
+        elif Port.upper() == "4":
             porta = self.getPorta().S4
-        if(porta == None):
+        if porta is None:
             raise TypeError("Porta não encontrada")
         return porta
-    
-
